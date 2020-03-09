1cb6578d8ee86329f1ccd86d73d7b182b8c35e14
Adeleke Temitayo <lekedevrepo@gmail.com>
2020-03-09 01:22:52 -0600

    add phaser logo to the assets folder and create BootScene.js file

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: ["babel-polyfill", "./src/index.js"],
 
   output: {
     path: path.resolve(__dirname, "build"),
