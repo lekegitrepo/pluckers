47f6958ed0a6288e4b750dc8890f48ac210ff061
Adeleke Temitayo <lekedevrepo@gmail.com>
2020-03-09 02:25:25 -0600

    import PreloaderScene in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import "phaser";
-import config from "./config/config";
 import BootScene from "./scenes/BootScene";
+import config from "./config/config";
+import PreloaderScene from "./Scenes/PreloaderScene";
 
 class Game extends Phaser.Game {
   constructor() {
     super(config);
 
     this.scene.add("Boot", BootScene);
+    this.scene.add("Preloader", PreloaderScene);
   }
 }
 
