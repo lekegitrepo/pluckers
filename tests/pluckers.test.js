7ceccde686bffb4a4ca0435f2885196bc0e75630
Adeleke Temitayo <lekedevrepo@gmail.com>
2020-03-18 11:16:10 -0600

    add GameController test

--- tests/pluckers.test.js
+++ tests/pluckers.test.js
@@ -1,31 +1,37 @@
 import Phaser from 'phaser';
-// const GameController = require('../src/game/game_controller');
-/* import Player from '../src/game/player';
+import GameController from '../src/game/game_controller';
+import Player from '../src/game/player';
 
-const player1 = new Player(
-  {
-    scene: '',
-    x: 750,
-    y: 300,
-    key: 'dude',
-    orient: 'left',
-    name: 'playerOne',
-  },
-  null,
-);
+jest.mock('../src/game/player');
 
-describe('Player', () => {
-  it('get player name, score', () => {
-    expect(player1.getPlayerName()).toBe('playerOne');
-  });
-}); */
+beforeEach(() => {
+  // Clear all instances and calls to constructor and all methods:
+  Player.mockClear();
+});
 
 describe('Phaser', () => {
-  it('check the instance of Phaser', () => {
+  it('Check the instance of Phaser', () => {
     expect(Phaser).toBeInstanceOf(Object);
   });
 
-  it('check Phaser version', () => {
+  it('Check Phaser version', () => {
     expect(Phaser).toHaveProperty('VERSION', '3.22.0');
+  });
+});
+
+describe('GameController', () => {
+  it('Check if the GameController called the class constructor', () => {
+    const player1 = new Player({}, null);
+    const player2 = new Player({}, null);
+    const gc = new GameController(player1, player2);
+    expect(Player).toHaveBeenCalledTimes(2);
+  });
+});
+
+describe('Player', () => {
+  it('get player name, score', () => {
+    const mockPlayerName = jest.fn('playerOne');
+    Player.prototype.getPlayerName = mockPlayerName;
+    expect(player.getPlayerName()).toBe('playerOne');
   });
 });
