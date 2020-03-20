837640e69733ec3746b14ca4ef8ffd7dc2ace57e
Adeleke Temitayo <lekedevrepo@gmail.com>
2020-03-20 06:09:33 -0600

    create scrollPanel function

--- src/scenes/ScoreBoard.js
+++ src/scenes/ScoreBoard.js
@@ -46,6 +46,64 @@ export default class ScoreBoard extends Phaser.Scene {
     );
   }
 
+  scrollPanel() {
+    this.print = this.add.text(0, 0, '');
+
+    let scrollablePanel = this.rexUI.add
+      .scrollablePanel({
+        x: 400,
+        y: 300,
+        width: 250,
+        height: 220,
+
+        scrollMode: 0,
+
+        background: this.rexUI.add.roundRectangle(
+          0,
+          0,
+          2,
+          2,
+          10,
+          COLOR_PRIMARY
+        ),
+
+        panel: {
+          child: this.rexUI.add.fixWidthSizer({
+            space: {
+              left: 3,
+              right: 3,
+              top: 3,
+              bottom: 3,
+              item: 8,
+              line: 8
+            }
+          }),
+
+          mask: {
+            padding: 1
+          }
+        },
+
+        slider: {
+          track: this.rexUI.add.roundRectangle(0, 0, 20, 10, 10, COLOR_DARK),
+          thumb: this.rexUI.add.roundRectangle(0, 0, 0, 0, 13, COLOR_LIGHT)
+        },
+
+        space: {
+          left: 10,
+          right: 10,
+          top: 10,
+          bottom: 10,
+
+          panel: 10
+        }
+      })
+      .layout();
+    //.drawBounds(this.add.graphics(), 0xff0000);
+
+    updatePanel(scrollablePanel, content);
+  }
+
   async getScore() {
     let y = 50;
     const scores = new PlayerInfo();
